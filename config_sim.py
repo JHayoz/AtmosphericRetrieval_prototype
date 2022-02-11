905602d59fa7a46b5ee51730f94feb33e6aed1df
JHayoz <jeanhayoz94@gmail.com>
2022-02-11 09:36:59 +0100

    Added simulation file

---
+++ config_sim.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  4 15:27:11 2021
+
+@author: jeanh
+"""
+
+import numpy as np
+import scipy.constants as cst
+from doubleRetrieval.util import save_1d_data
+
+NAME = 'spectrum'
+VERSION = '01'
+NAME += '_v' + VERSION
+
+MODEL = 'chem_equ'  # 'free' or 'chem_equ'
+OUTPUT_FORMAT = 'datalike'  # determines whether output has same bins as data ('datalike') or if it still contains its extensions
+INSTRUMENT = 'SINFONI'
+
+USE_PRIOR = None
+
+IPAGATE_ROUTE = '/home/ipa/quanz/user_accounts/'
+
+OUTPUT_DIR = IPAGATE_ROUTE + ''
+OUTPUT_DIR += NAME
+
+# where to get data from to make synthetic data
+INPUT_FILE = IPAGATE_ROUTE + ''
+
+CC_WVL_DIR = INPUT_FILE + 'CC_data/CC_SINFONI'
+
+RES_WVL_DIR = INPUT_FILE + 'RES_data/Flux'
+RES_COV_DIR = INPUT_FILE + 'RES_data/Error'
+
+FILTER_DIR = INPUT_FILE + 'PHOT_data/filter/'
+PHOT_DATA_FILE = INPUT_FILE + 'PHOT_data/flux/'
+
+EXTERNAL_PROFILES = False
+
+MODE = 'lbl'
+LBL_SAMPLING = None
+CONVERT_SINFONI_UNITS = True
+
+RESOLUTION = 8000
+
+WLEN_BORDERS = [2.0880364682002703, 2.4506398060442036]
+WLEN_BORDERS = [0.8, 5]
+EXTENSIONS = None
+
+# ABUNDANCES = ['H2O_main_iso','CH4_main_iso', 'CO_main_iso', 'CO2_main_iso']
+ABUNDANCES = ['C/O', 'FeHs']
+
+pc_to_m = 3.086 * 1e16
+distance_pc = 19.7538
+DISTANCE = distance_pc * pc_to_m
+
+RADIUS_J = 69911 * 1000
+MASS_J = 1.898 * 1e27
+
+TEMP_PARAMS = {}
+
+TEMP_PARAMS['log_gamma'] = np.log10(0.4)
+TEMP_PARAMS['t_int'] = 200.
+TEMP_PARAMS['t_equ'] = 1742
+TEMP_PARAMS['log_R'] = np.log10(1.36)
+TEMP_PARAMS['log_gravity'] = 4.35
+TEMP_PARAMS['log_kappa_IR'] = np.log10(0.01)
+TEMP_PARAMS['P0'] = 2
+
+CLOUDS_PARAMS = {}
+"""
+CLOUDS_PARAMS['log_Pcloud']     = np.log10(0.5)
+"""
+
+AB_METALS = {}
+# AB_METALS['H2O_main_iso']   = -2.4
+# AB_METALS['CO_main_iso']    = -3.3
+# AB_METALS['CH4_main_iso']   = -4.5
+# AB_METALS['CO2_main_iso']   = -4.2
+AB_METALS['C/O'] = 1
+AB_METALS['FeHs'] = 0.66
+
+PHYSICAL_PARAMS = {}
+# PHYSICAL_PARAMS['rot_vel'] = 25
+PHYSICAL_PARAMS['rot_vel'] = 0
+
+DATA_PARAMS = {}
+DATA_PARAMS.update(TEMP_PARAMS)
+DATA_PARAMS.update(AB_METALS)
+DATA_PARAMS.update(CLOUDS_PARAMS)
+DATA_PARAMS.update(PHYSICAL_PARAMS)
+
+WINDOW_LENGTH_LBL = 60  # now referring to the length of the window for the only gaussian filter, for sinfoni data
+WINDOW_LENGTH_CK = 41
+
+RV = 31
+NOISE = 0.05
+NOISE_RES = 0
+SNR = 0
+
+CONFIG_DICT = {}
+CONFIG_DICT['ALL_PARAMS'] = ABUNDANCES
+CONFIG_DICT['ABUNDANCES'] = ABUNDANCES
+CONFIG_DICT['NEEDED_LINE_SPECIES'] = ABUNDANCES
+CONFIG_DICT['TEMPS'] = ['log_gamma', 't_int', 't_equ', 'log_gravity', 'log_kappa_IR', 'log_R', 'P0']
+CONFIG_DICT['CLOUDS'] = CLOUDS_PARAMS.keys()
+# CONFIG_DICT['CLOUDS_OPACITIES'] = CLOUDS_OPACITIES
+CONFIG_DICT['UNSEARCHED_ABUNDANCES'] = []
+CONFIG_DICT['UNSEARCHED_TEMPS'] = []
+CONFIG_DICT['UNSEARCHED_CLOUDS'] = []
+CONFIG_DICT['PARAMS_NAMES'] = []
+CONFIG_DICT['UNSEARCHED_PARAMS'] = []
+CONFIG_DICT['RVMAX'] = 0
+CONFIG_DICT['RVMIN'] = 0
+CONFIG_DICT['DRV'] = 0
+CONFIG_DICT['DISTANCE'] = DISTANCE
+CONFIG_DICT['WIN_LEN'] = WINDOW_LENGTH_LBL
+CONFIG_DICT['LBL_SAMPLING'] = LBL_SAMPLING
+CONFIG_DICT['CONVERT_SINFONI_UNITS'] = CONVERT_SINFONI_UNITS
+CONFIG_DICT['WRITE_THRESHOLD'] = 0
+CONFIG_DICT['DATA_PARAMS'] = DATA_PARAMS
---
+++ sim_data.py
@@ -0,0 +1,102 @@
+import numpy as np
+import matplotlib
+
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+import sys
+import os
+
+os.environ["pRT_input_data_path"] = "/home/ipa/quanz/shared/petitRADTRANS/input_data"
+sys.path.append("/home/ipa/quanz/shared/petitRADTRANS/")
+from petitRADTRANS import radtrans as rt
+from petitRADTRANS import nat_cst as nc
+
+from doubleRetrieval.forward_model import ForwardModel
+from doubleRetrieval.plotting import plot_data, plot_profiles
+from doubleRetrieval.util import convert_units, save_photometry, save_spectrum, trim_spectrum, calc_cov_matrix, \
+    save_spectra, save_lines
+from doubleRetrieval.rebin import rebin_to_CC, rebin, doppler_shift, rebin_to_RES, rebin_to_PHOT, only_gaussian_filter
+from doubleRetrieval.data2 import Data
+from doubleRetrieval.rotbroad_utils import add_rot_broad, cut_spectrum
+from config_sim import *
+
+import csv
+import pickle
+import os
+
+if not os.path.exists(OUTPUT_DIR):
+    os.mkdir(OUTPUT_DIR)
+
+import shutil
+
+cwd = os.getcwd()
+source = cwd + '/sim_config2.py'
+destination = OUTPUT_DIR + '/sim_config2_copy.py'
+shutil.copyfile(source, destination)
+print('Config file copied')
+
+# redesign how to call data object for simulation
+# idea: import real data, and copy format
+
+resolution = 4000
+wlen_borders = [2,4]
+stepsize = (wlen_borders[0]+wlen_borders[1])/2/resolution
+N_points = (wlen_borders[1]-wlen_borders[0])/stepsize
+wlen_data = np.linspace(wlen_borders[0],wlen_borders[1],N_points)
+
+instruments = {}
+instruments['SINFONI'] = wlen_data
+
+forwardmodel_lbl = ForwardModel(
+    wlen_borders=wlen_borders,
+    max_wlen_stepsize=stepsize,
+    mode='lbl',
+    line_opacities=ABUNDANCES,
+    model=MODEL,
+    max_RV=0,
+    max_winlen=WINDOW_LENGTH_LBL
+)
+forwardmodel_lbl.calc_rt_obj(lbl_sampling=LBL_SAMPLING)
+
+wlen_lbl, flux_lbl, pressures, temperatures, abundances = forwardmodel_lbl.calc_spectrum(
+    ab_metals=AB_METALS,
+    temp_params=TEMP_PARAMS,
+    clouds_params=CLOUDS_PARAMS,
+    external_pt_profile=None,
+    return_profiles=True)
+
+plot_profiles(
+    pressures,
+    temperatures=temperatures,
+    abundances=abundances,
+    output_dir=OUTPUT_DIR,
+    fontsize=20)
+
+CC_wlen_shifted, CC_flux_shifted = doppler_shift(wlen_lbl, flux_lbl, RV)
+CC_wlen_removed, CC_flux_removed, CC_wlen_rebin, CC_flux_rebin, sgfilter = {}, {}, {}, {}, {}
+
+if CONVERT_SINFONI_UNITS:
+    CC_wlen_shifted, CC_flux_shifted = convert_units(CC_wlen_shifted, CC_flux_shifted, TEMP_PARAMS['log_R'], DISTANCE)
+else:
+    CC_wlen_shifted = 1e4 * CC_wlen_shifted
+
+for key in instruments.keys():
+    # rebinning
+    CC_wlen_rebin[key], CC_flux_rebin[key] = rebin(CC_wlen_shifted, CC_flux_shifted, instruments[key])
+
+    if PHYSICAL_PARAMS['rot_vel'] != 0:
+        CC_wlen_rebin[key], CC_flux_rebin[key] = add_rot_broad(CC_wlen_rebin[key], CC_flux_rebin[key],
+                                                               PHYSICAL_PARAMS['rot_vel'], method='fast',
+                                                               edgeHandling='cut')
+
+    # remove continuum with filter
+    wlen_after = None
+    if OUTPUT_FORMAT == 'datalike':
+        wlen_after = CC_wlen_data[key]
+    CC_wlen_removed[key], CC_flux_removed[key], sgfilter[key] = only_gaussian_filter(CC_wlen_rebin[key],
+                                                                                     CC_flux_rebin[key],
+                                                                                     sigma=WINDOW_LENGTH_LBL,
+                                                                                     wlen_after=wlen_after)
+
+save_spectra(CC_wlen_removed, CC_flux_removed, save_dir=OUTPUT_DIR + '/CC_spectrum', save_name='')
